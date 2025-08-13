bd169a4b4c72cdbc6969d53ab64442ac6e14db3b
Ruben Serra <102155988+rserravi@users.noreply.github.com>
2025-08-13 20:15:32 +0200

    refactor: move character helpers and localize traits

---
+++ src/data/traits.es.ts
@@ -0,0 +1,13 @@
+// Lista ampliada (Brady & Lee, pares opuestos; usamos cada adjetivo como sugerencia)
+export const TRAIT_SUGGESTIONS = [
+  'parco','pródigo','gentil','violento','alegre','lánguido','delicado','estúpido',
+  'generoso','avaro','claro','confuso','gregario','aislado','moral','inmoral',
+  'crédulo','incrédulo','sano','enfermo','ingenuo','malicioso','cruel','benevolente',
+  'dubitativo','impulsivo','sucio','inmaculado','inteligente','tonto','gracioso','apático',
+  'valiente','cobarde','fanfarrón','humilde','obstinado','dócil','justo','injusto',
+  'optimista','pesimista','tranquilo','nervioso','sensible','insensible','arrogante','cortés',
+  'extravagante','mesurado','sencillo','complejo','vulgar','noble','lúcido','alienado',
+  'misterioso','evidente','impetuoso','sereno','egoísta','altruista','leal','desleal',
+  'locuaz','taciturno','pretencioso','modesto','natural','afectado','torpe','hábil',
+  'astuto','franco','histérico','plácido','galante','rudo','activo','perezoso'
+] as const;
